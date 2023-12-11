caadf4da4c62d8caad48ec01564e8b7c2a20c8b6
C0de871 <muhammadaydi3@gmail.com>
2023-12-12 00:58:28 +0300

    this is the log in for the back end of the project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author 1
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Login user1 = new Login();
+        Login user2 = new Login();
+
+           user1.register();
+        user1.register();
+        user1.login();
+        user1.login();
+    }
+}
+//L was here
+//Code 871
+//The Only
+//Adnan.add
