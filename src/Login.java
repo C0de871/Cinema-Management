31c8a5f7b5e6f26fb48d19b98adb0651a11cb6bb
LENONO <abdallaalkasm9@gmail.com>
2023-12-12 11:39:36 +0200

    abd

--- src/Login.java
+++ src/Login.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class Login implements Serializable {
     //just comment
     //second comment
+    //third comment
     /*تم اختيار الماب  لتخزين بيانات المستخدمين في  البرنامج لأنه يوفر طريقة لتخزين الأزواج المفتاح-القيمة. في هذه الحالة، يتم استخدام الماب لتخزين عناوين البريد الإلكتروني للمستخدمين كمفتاح وكلمات المرور كقيمة. هذا يسمح بسهولة الوصول إلى بيانات المستخدم والتحقق من صحة بيانات  .*/
     private Map<String, String> userCredentials;//تعريف ماب لتخزين البيانات اعتمادا للمستخدم
     private Pattern passwordPattern;// تعريف نمط لفحص صحة كلمة المرور
