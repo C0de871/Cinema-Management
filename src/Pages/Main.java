01097f3246e77588e82b3916dd8bfd245fd8ef2d
C0de871 <muhammadaydi3@gmail.com>
2023-12-29 00:16:09 +0300

    remove the duplicate main in my interfaces not in the backend and now to run the interfaces you should run int from Main clas in the interfaces package

---
+++ src/Pages/Main.java
@@ -0,0 +1,24 @@
+package Pages;
+
+import Login.LogInUp;
+
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LogInUp().setVisible(true);
+            }
+        });
+        //uncomment this section to add the body to the scrollpanel
+
+//        JScrollPane scrollPane= new ScrollPaneWin11();
+//        scrollPane.setViewportView(bodyPanel);
+//        scrollPane.setBorder(BorderFactory.createLineBorder(Color.decode("#222631"), 2));
+//        scrollPane.setBackground(Color.decode("#222631"));
+//        scrollPane.setForeground(Color.decode("#222631"));
+//        parentPanel.add(scrollPane, "push,grow");
+
+
+
+    }
+}
